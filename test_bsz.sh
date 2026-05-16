80899a0480fcac0f4dc97396a1dad8b393efa8f2
wangchangyue <wangchangyue@bytedance.com>
2026-05-16 10:49:03 +0000

    tt

--- test_bsz.sh
+++ test_bsz.sh
@@ -4,6 +4,5 @@ deepspeed  --master_port=11470 test_bsz.py \
     --model_path /mnt/hdfs/wangchangyue/LLM/Qwen3-8B \
     --num_mem 256 \
     --max_length 2048 \
-    --test_mode pretrain \
     --start_bsz 2 \
     --max_bsz_limit 16
