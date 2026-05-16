27e76589f62c3fdc34d7a5871a101edf8b75a314
bebr2 <1215427670@qq.com>
2026-05-16 22:16:37 +0800

    ss

--- dl.sh
+++ dl.sh
@@ -1,3 +1,11 @@
-wget https://cloud.tsinghua.edu.cn/f/97123770ec724316a02e/?dl=1 -O /mnt/hdfs/wangchangyue/500xCompressor/output/processed/finetune.jsonl
+# wget https://cloud.tsinghua.edu.cn/f/97123770ec724316a02e/?dl=1 -O /mnt/hdfs/wangchangyue/500xCompressor/output/processed/finetune.jsonl
 
-wget https://cloud.tsinghua.edu.cn/f/768f9e859c1748cf9e59/?dl=1 -O /mnt/hdfs/wangchangyue/500xCompressor/output/processed/pretrain.txt
+# wget https://cloud.tsinghua.edu.cn/f/768f9e859c1748cf9e59/?dl=1 -O /mnt/hdfs/wangchangyue/500xCompressor/output/processed/pretrain.txt
+
+wget https://cloud.tsinghua.edu.cn/f/33880d1745724317a77d/?dl=1 -O /mnt/hdfs/wangchangyue/500xCompressor/data/test.txt
+
+wget https://cloud.tsinghua.edu.cn/f/146b2f1006f34dcdbe91/?dl=1 -O /mnt/hdfs/wangchangyue/500xCompressor/data/test_qa.jsonl
+
+wget https://cloud.tsinghua.edu.cn/f/d4548ce8a58a4c2cbc83/?dl=1 -O /mnt/hdfs/wangchangyue/500xCompressor/data/train.txt
+
+wget https://cloud.tsinghua.edu.cn/f/e37b4a7a7f1a4c3e9098/?dl=1 -O /mnt/hdfs/wangchangyue/500xCompressor/data/train_qa.jsonl
