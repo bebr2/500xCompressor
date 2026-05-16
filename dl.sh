22aeb4ef51516bd0cedabbb512bf1ff20b7a265a
bebr2 <1215427670@qq.com>
2026-05-16 16:47:05 +0800

    ss

---
+++ dl.sh
@@ -0,0 +1,3 @@
+wget https://cloud.tsinghua.edu.cn/f/97123770ec724316a02e/?dl=1 -O /mnt/hdfs/wangchangyue/500xCompressor/output/processed/finetune.jsonl
+
+wget https://cloud.tsinghua.edu.cn/f/768f9e859c1748cf9e59/?dl=1 -O /mnt/hdfs/wangchangyue/500xCompressor/output/processed/pretrain.txt
